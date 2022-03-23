c986e975d3c8dddaed9a8977f40c69537fef88e6
Emre Tepe <emretepedev@gmail.com>
2022-03-23 03:25:56 +0300

    initial commit

---
+++ src/extension.ts
@@ -0,0 +1,30 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+function use(x: vscode.Terminal) {
+  if (process.platform === "win32") {
+    x.sendText(`solc-select use $(Get-Content .svmrc).replace( 'v', '' )`);
+  }
+  else {
+    x.sendText('solc-select use $(cat .svmrc)');
+  }
+}
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  const terminals = (<any>vscode.window).terminals;
+  if (terminals.length) {
+    terminals.forEach(function switchNode(t: vscode.Terminal) {
+      use(t);
+    });
+  }
+  (<any>vscode.window).onDidOpenTerminal((e: vscode.Terminal) => {
+    use(e);
+  });
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
